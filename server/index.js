9ef14ba19155b6c352c60dc0f2d165c1881e92c8
Harim Jeong <harim97714@naver.com>
2021-12-09 00:04:00 +0900

    백엔드 생성 및 MongoDB 연결

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 5001;
+
+const config = require('./config/key');
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
